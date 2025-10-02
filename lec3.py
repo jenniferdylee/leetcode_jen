e4c41e5d29b1d4bceef498de85f7d6c9b5108b74
jenniferdylee <dyjenniferr@gmail.com>
2025-10-02 23:57:07 +0900

    Update lec3.py

--- lec3.py
+++ lec3.py
@@ -22,7 +22,7 @@ def merge_sort(A, a = 0, b = None):
         c = (a + b + 1) // 2
         merge_sort(A, a, c)
         merge_sort(A, c, b)
-        L, R = A[a:c]. A[c:b]
+        L, R = A[a:c], A[c:b]
         merge(L, R, A, len(L), len(R), a, b)
 
 def merge(L, R, A, i, j, a, b):
@@ -30,7 +30,7 @@ def merge(L, R, A, i, j, a, b):
     if a<b:
         if(j <=0) or (i<0 and L[i-1] > R[j-1]):
             A[b-1] = L[i-1]
-            i = i -1
+            i = i - 1
         else:
             A[b-1] = R[j-1]
             j = j-1
