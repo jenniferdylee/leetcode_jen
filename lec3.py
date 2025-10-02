db83dd44a857cafece6a2071be88506a4207761b
jenniferdylee <dyjenniferr@gmail.com>
2025-10-02 14:25:16 +0000

    selection sort added

--- lec3.py
+++ lec3.py
@@ -5,3 +5,11 @@ def prefix_max(A, i):
         if A[i] < A[j]:
             return j
     return i
+
+def selection_sort(A, i = None):
+    '''Sort A{:i+1}'''
+    if i is None: i = len(A) - 1
+    if i > 0:
+        j = prefix_max(A, i)
+        A[i], A[j], = A[j], A[i]
+        selection_sort(A, i-1)
